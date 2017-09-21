cb4aa19923fc5ea009fa332b03e93db9461c8c70
Zhitao <zw56@njit.edu>
2017-09-20 23:47:45 -0400

    Initial Commit

---
+++ Social/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Social
+//
+//  Created by Zhitao Wang on 9/20/17.
+//  Copyright © 2017 Zhitao Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Social/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Social
+//
+//  Created by Zhitao Wang on 9/20/17.
+//  Copyright © 2017 Zhitao Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
